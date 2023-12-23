01e8980649070f493777cfa75c352c07224a0e40
Tim Kinsman <tkinsm@gmail.com>
2023-12-23 19:42:17 +1030

    fix: change theme icons

--- src/features/theme/components/ThemeToggle.tsx
+++ src/features/theme/components/ThemeToggle.tsx
@@ -24,25 +24,82 @@ export const ThemeToggle = () => {
       onClick={() => setTheme(isDarkTheme ? 'light' : 'dark')}
     >
       <svg
-        id="theme-toggle-dark-icon"
-        className={clsx('w-5 h-5', { hidden: isDarkTheme })}
-        fill="currentColor"
-        viewBox="0 0 20 20"
+        viewBox="0 0 48 48"
         xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-      </svg>
-      <svg
+        fill="none"
+        stroke="currentColor"
         id="theme-toggle-light-icon"
         className={clsx('w-5 h-5', { hidden: !isDarkTheme })}
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          d="M6.40039 24H9.60039"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+        <path
+          d="M11.5557 11.5557L13.8181 13.8181"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+        <path
+          d="M24 6.40039V9.60039"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+        <path
+          d="M36.445 11.5557L34.1826 13.8181"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+        <path
+          d="M41.6004 24H38.4004"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+        <path
+          d="M36.445 36.445L34.1826 34.1826"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+        <path
+          d="M24 41.6004V38.4004"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+        <path
+          d="M11.5557 36.445L13.8181 34.1826"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+        <path
+          d="M24.0004 33.6004C29.3023 33.6004 33.6004 29.3023 33.6004 24.0004C33.6004 18.6985 29.3023 14.4004 24.0004 14.4004C18.6985 14.4004 14.4004 18.6985 14.4004 24.0004C14.4004 29.3023 18.6985 33.6004 24.0004 33.6004Z"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+      </svg>
+
+      <svg
+        id="theme-toggle-dark-icon"
+        viewBox="0 0 48 48"
+        className={clsx('w-5 h-5', { hidden: isDarkTheme })}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        stroke="currentColor"
+      >
+        <path
+          d="M35.2 28.7996C26.4 28.7996 19.2 21.5996 19.2 12.7996C19.2 11.3596 19.44 9.91957 19.76 8.55957C12.96 10.3996 8 16.6396 8 23.9996C8 32.7996 15.2 39.9996 24 39.9996C31.36 39.9996 37.6 35.0396 39.44 28.2396C38.08 28.5596 36.64 28.7996 35.2 28.7996Z"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         ></path>
       </svg>
     </button>
