b1c694541d9a863a2069ce44243ab6f034792f51
timkinsman <tkinsm@gmail.com>
2024-02-03 19:09:02 +1030

    feat: add google analytics

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,23 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import svgr from 'vite-plugin-svgr';
+import { VitePluginRadar } from 'vite-plugin-radar';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react(), svgr()],
+    plugins: [
+      react(),
+      svgr(),
+      VitePluginRadar({
+        // Google Analytics tag injection
+        analytics: { id: env.VITE_ANALYTICS_ID },
+        // Google Tag Manager (multiple tag can be set with an array)
+        gtm: [{ id: env.VITE_GTM_ID }],
+      }),
+    ],
     server: {
       open: true,
       port: Number(env.VITE_PORT) || 3000,
